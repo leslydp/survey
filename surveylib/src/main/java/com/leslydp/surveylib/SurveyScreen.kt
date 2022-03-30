83f5fec0cf88829369481431a4634e1e6cbf743b
Lesly Delgado <leslydp00@gmail.com>
2022-03-30 16:30:19 -0400

    slider commit

--- surveylib/src/main/java/com/leslydp/surveylib/SurveyScreen.kt
+++ surveylib/src/main/java/com/leslydp/surveylib/SurveyScreen.kt
@@ -2,16 +2,27 @@ package com.leslydp.surveylib
 
 import android.util.Log
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.leslydp.surveylib.model.Answer
 import com.leslydp.surveylib.model.Question
@@ -22,12 +33,10 @@ import com.leslydp.surveylib.model.Question
 )
 @Composable
 fun JetsurveyScreen(
-    list: List<Question>,
+    questions: List<Question>,
     onAnswer: (Answer) -> Unit
 ) {
-    //var checkedState by remember{mutableStateOf(false)}
-
-    list.forEach { question ->
+    questions.forEach { question ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -38,27 +47,37 @@ fun JetsurveyScreen(
                 Spacer(modifier = Modifier.height(32.dp))
                 QuestionTitle(question.questionName)
                 Spacer(modifier = Modifier.height(24.dp))
-                var respuesta: ArrayList<Byte> = ArrayList<Byte>()
-                question.options.forEach {
-                    respuesta.add(0)
-                }
+
                 Log.d("myTag2", question.questionName)
                 //QuestionTitle(question.questionName)
                 Log.d("tag", question.options.size.toString())
                 when (question.id) {
                     1 -> {
+                        val respuesta = mutableListOf<Byte>()
+
                         MultipleChoiceQuestion(question.options, onAnswerSelected = { idq, valor ->
                             val compareTo = valor.compareTo(false)
-                            respuesta.set(idq, compareTo.toByte())
+                            respuesta[idq] = compareTo.toByte()
                             Log.d("myTag", respuesta.toString())
 
                         })
                     }
+                    2 -> {
+                        val respuesta = mutableListOf<Byte>()
+
+                        SingleChoiceQuestion(question.options, onAnswerSelected = { id ->
+                            if (respuesta.indexOf(1) > -1)
+                                respuesta[respuesta.indexOf(1)] = 0
+                            respuesta[id]=1
+                            Log.d("myTag", respuesta.toString())
+
+                        })
+                    }
+
                 }
             }
         }
     }
-
 }
 
 @Composable
@@ -88,23 +107,14 @@ private fun QuestionTitle(title: String) {
 
 @Composable
 private fun MultipleChoiceQuestion(
-    //possibleAnswer: PossibleAnswer.MultipleChoice,
-    //answer: Answer.MultipleChoice?,
+
     options: List<String>,
     onAnswerSelected: (Int, Boolean) -> Unit,
-    //onAnswerSelected: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    //var checkedState by remember{mutableStateOf(false)}
-    //val (value, setValue) = remember { mutableStateOf(Boolean[]) }
-    //val options = possibleAnswer.optionsStringRes.associateBy { stringResource(id = it) }
+
     Column(modifier = modifier) {
         for (option in options) {
-            // } //{
-            // val selectedOption = answer?.answersStringRes?.contains(option.value)
-            // val selectedOption = options.indexOf(option)
-            // mutableStateOf(selectedOption ?: false)
-            // }
             var checkedState by remember { mutableStateOf(false) }
             val answerBorderColor = if (checkedState) {
                 MaterialTheme.colors.primary.copy(alpha = 0.5f)
@@ -158,3 +168,124 @@ private fun MultipleChoiceQuestion(
         }
     }
 }
+
+@Composable
+private fun SingleChoiceQuestion(
+    options: List<String>,
+    onAnswerSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var checkedState by remember { mutableStateOf(-1) }
+
+    Column(modifier = modifier) {
+        options.forEach { text ->
+
+            val optionSelected = options.indexOf(text) == checkedState
+
+            val answerBorderColor = if (optionSelected) {
+                MaterialTheme.colors.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+            }
+            val answerBackgroundColor = if (optionSelected) {
+                MaterialTheme.colors.primary.copy(alpha = 0.12f)
+            } else {
+                MaterialTheme.colors.background
+            }
+            Surface(
+                shape = MaterialTheme.shapes.small,
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = answerBorderColor
+                ),
+                modifier = Modifier.padding(vertical = 8.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .selectable(
+                            selected = options.indexOf(text) == checkedState,
+                            onClick = {
+                                checkedState = options.indexOf(text)
+                                onAnswerSelected(options.indexOf(text))
+                            }
+                        )
+                        .background(answerBackgroundColor)
+                        .padding(vertical = 16.dp, horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = text
+                    )
+
+                    RadioButton(
+                        selected = options.indexOf(text) == checkedState,
+                        onClick = {
+                            checkedState = options.indexOf(text)
+                            onAnswerSelected(options.indexOf(text))
+                        },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = MaterialTheme.colors.primary
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SliderQuestion(
+    //possibleAnswer: PossibleAnswer.Slider,
+    //answer: Answer.Slider?,
+    options: List<String>,
+    onAnswerSelected: (Float) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var sliderPosition by remember {
+        mutableStateOf(5.5f)
+    }
+    Row(modifier = modifier) {
+
+        Slider(
+            value = sliderPosition,
+            onValueChange = {
+                sliderPosition = it
+                onAnswerSelected(it)
+            },
+            valueRange = 100/options.size,
+            steps = possibleAnswer.steps,
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp)
+        )
+    }
+    Row {
+        Text(
+            text = stringResource(id = possibleAnswer.startText),
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.8f)
+        )
+        Text(
+            text = stringResource(id = possibleAnswer.neutralText),
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.8f)
+        )
+        Text(
+            text = stringResource(id = possibleAnswer.endText),
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.End,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.8f)
+        )
+    }
+}
+
